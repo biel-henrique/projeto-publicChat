f30772e325a2de2a5c991575fe3a572662ddd648
Gabriel Henrique Cruz <gabrielhenrrique_cruz@hotmail.com>
2024-10-29 23:42:18 -0300

    fix: optimization

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const initLink ="https://mock-api.driven.com.br/api/v6/uol/participants/b898fc0b-16b8-45f9-912e-7ababdc3308d";
-const connectLink ="https://mock-api.driven.com.br/api/v6/uol/status/b898fc0b-16b8-45f9-912e-7ababdc3308d";
-const msgLink ="https://mock-api.driven.com.br/api/v6/uol/messages/b898fc0b-16b8-45f9-912e-7ababdc3308d";
+const initLink ="https://mock-api.driven.com.br/api/v6/uol/participants/79ad69c5-8da1-4f20-adee-fd19ed62601e";
+const connectLink ="https://mock-api.driven.com.br/api/v6/uol/status/79ad69c5-8da1-4f20-adee-fd19ed62601e";
+const msgLink ="https://mock-api.driven.com.br/api/v6/uol/messages/79ad69c5-8da1-4f20-adee-fd19ed62601e";
 
 let userName = {
     name: inputName(),
@@ -62,14 +62,11 @@ function getName() {
 }
 
 function addUsersMenu(element) {
-  console.log(element.data)
   const getDiv = document.querySelector(".users");
   let check = document.querySelector('.check')
   let people = document.querySelector('.allPeople')
   let whichUser = configUser()
   let content = "";
-
-  console.log(whichUser)
 
   while (getDiv.children.length > 1) {
     getDiv.removeChild(getDiv.lastChild);
@@ -114,7 +111,6 @@ function configUser() {
   );
 
   if(whichUser === null) {
-    console.log('entrou')
     allPeople.classList.add('selectedParentUser')
     allPeople.lastElementChild.classList.remove('hidden')
     allPeople.lastElementChild.classList.add('selected')
@@ -222,20 +218,20 @@ function getChatBox() {
   promise.then(updtFeed)
 }
 
-function justArrived(msg, updtChat, i, getUl, lastMessage) {
+function justArrived(msg, message, getUl, lastMessage) {
   msg.classList.add('joinExit');
   msg.innerHTML = `
-    <div class="content"><span style="color: gray;">(${updtChat[i].time}) </span><strong>${updtChat[i].from}</strong> ${updtChat[i].text}</div>`;
+    <div class="content"><span style="color: gray;">(${message.time}) </span><strong>${message.from}</strong> ${message.text}</div>`;
   getUl.appendChild(msg);
   if(lastMessage != null) {
     lastMessage.scrollIntoView({ behavior: "smooth" }); 
   }
 }
 
-function loadMessages(msg, char, updtChat, i, getUl, lastMessage) {
+function loadMessages(msg, char, message, getUl, lastMessage) {
   msg.classList.add(char);
   msg.innerHTML = `
-    <div class="content"><span style="color: gray;">(${updtChat[i].time}) </span><strong>${updtChat[i].from}</strong> para <strong>${updtChat[i].to}</strong>: ${updtChat[i].text}</div>`;
+    <div class="content"><span style="color: gray;">(${message.time}) </span><strong>${message.from}</strong> para <strong>${message.to}</strong>: ${message.text}</div>`;
   getUl.appendChild(msg);
   if(lastMessage != null) {
     lastMessage.scrollIntoView({ behavior: "smooth" });
@@ -249,25 +245,25 @@ function updtFeed(element) {
     (user) => user.from === userName.name || user.to === userName.name || user.to === 'Todos' || user.type === 'message'
   );
 
-  for(let i = 0; i < updtChat.length; i++) {
-    let messageKey = `${updtChat[i].from}-${updtChat[i].to}-${updtChat[i].text}-${updtChat[i].time}`;
-
-    if(!compareMsg.has(messageKey)) {
+  updtChat.forEach((message) => {
+    let messageKey = `${message.from}-${message.to}-${message.text}-${message.time}`;
+  
+    if (!compareMsg.has(messageKey)) {
       let lastMessage = getUl.lastElementChild;
       let msg = document.createElement("li");
-
-      if(updtChat[i].type === 'status'){
-        justArrived(msg, updtChat, i, getUl, lastMessage)
-      }else if(updtChat[i].type === 'private_message' && updtChat[i].to != 'Todos') {
-        loadMessages(msg, char = 'private', updtChat, i, getUl, lastMessage)
-      }else {
-        loadMessages(msg, char = 'common', updtChat, i, getUl, lastMessage)
+  
+      if (message.type === 'status') {
+        justArrived(msg, message, getUl, lastMessage);
+      } else if (message.type === 'private_message' && message.to != 'Todos') {
+        loadMessages(msg, char = 'private', message, getUl, lastMessage);
+      } else {
+        loadMessages(msg, char = 'common', message, getUl, lastMessage);
       }
+  
       getUl.appendChild(msg);
-      compareMsg.add(messageKey)
+      compareMsg.add(messageKey);
     }
-    
-  }
+  });
 }
 
 setInterval(checkStatus, 1000)
